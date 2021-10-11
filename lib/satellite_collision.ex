5fdb36cc113afac453414b442c28f9c36f66ed95
brandynbennett <brandynlbennett@gmail.com>
2021-10-11 14:50:18 -0600

    pull data from api

--- lib/satellite_collision.ex
+++ lib/satellite_collision.ex
@@ -6,6 +6,22 @@ defmodule SatelliteCollision do
   @doc """
   Determine if 2 satellites are too close together
   """
+  def will_collide?(threshold) do
+    response = HTTPoison.get!("https://api.spacexdata.com/v4/starlink")
+
+    Jason.decode!(response.body)
+    |> Enum.filter(fn item -> item["height_km"] end)
+    |> Enum.map(fn item ->
+      %Satellite{
+        height: item["height_km"],
+        latitude: item["latitude"],
+        longitude: item["longitude"],
+        eccentricity: item["spaceTrack"]["ECCENTRICITY"]
+      }
+    end)
+    |> will_collide?(threshold)
+  end
+
   def will_collide?(satellites, threshold) when is_list(satellites) do
     Enum.any?(satellites, fn satellite1 ->
       Enum.any?(List.delete(satellites, satellite1), fn satellite2 ->
--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule SatelliteCollision.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      applications: [:httpoison]
     ]
   end
 
@@ -23,7 +24,9 @@ defmodule SatelliteCollision.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:math, "~> 0.7.0"}
+      {:math, "~> 0.7.0"},
+      {:httpoison, "~> 1.7"},
+      {:jason, "~> 1.2"}
     ]
   end
 end
