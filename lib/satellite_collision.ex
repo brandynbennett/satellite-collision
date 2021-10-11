d96aa73bb53f16beb3c30144b074c6998b419317
brandynbennett <brandynlbennett@gmail.com>
2021-10-11 14:17:25 -0600

    Test if any satellite will collide

--- lib/satellite_collision.ex
+++ lib/satellite_collision.ex
@@ -6,11 +6,18 @@ defmodule SatelliteCollision do
   @doc """
   Determine if 2 satellites are too close together
   """
+  def will_collide?(satellites, threshold) when is_list(satellites) do
+    Enum.any?(satellites, fn satellite1 ->
+      Enum.any?(List.delete(satellites, satellite1), fn satellite2 ->
+        will_collide?(satellite1, satellite2, threshold)
+      end)
+    end)
+  end
+
   def will_collide?(satellite1, satellite2, threshold) do
     point1 = Satellite.to_cartesian(satellite1)
     point2 = Satellite.to_cartesian(satellite2)
     distance = Point.distance(point1, point2)
-    IO.inspect(distance, label: "distance")
     distance <= threshold
   end
 end
--- test/satellite_collision_test.exs
+++ test/satellite_collision_test.exs
@@ -36,4 +36,29 @@ defmodule SatelliteCollisionTest do
 
     assert SatelliteCollision.will_collide?(satellite1, satellite2, 700) == true
   end
+
+  test "will_collide? true if any satellites will collide" do
+    satellites = [
+      %Satellite{
+        eccentricity: 0.0001131,
+        height: 556.0281731621435,
+        latitude: -27.73707947914896,
+        longitude: -97.17265433167117
+      },
+      %Satellite{
+        eccentricity: 0.0003191,
+        height: 556.0281731621435,
+        latitude: -27.73707947914896,
+        longitude: -97.17265433167117
+      },
+      %Satellite{
+        eccentricity: 0.0003191,
+        height: 408.25189267837504,
+        latitude: -4.444904136046365,
+        longitude: 115.67730280679655
+      }
+    ]
+
+    assert SatelliteCollision.will_collide?(satellites, 700) == true
+  end
 end
