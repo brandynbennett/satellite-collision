774cf147642c69a142d095b6e995ebddb4873d2b
brandynbennett <brandynlbennett@gmail.com>
2021-10-11 12:42:46 -0600

    create projects

---
+++ lib/satellite_collision.ex
@@ -0,0 +1,18 @@
+defmodule SatelliteCollision do
+  @moduledoc """
+  Documentation for `SatelliteCollision`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SatelliteCollision.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/satellite_collision_test.exs
@@ -0,0 +1,8 @@
+defmodule SatelliteCollisionTest do
+  use ExUnit.Case
+  doctest SatelliteCollision
+
+  test "greets the world" do
+    assert SatelliteCollision.hello() == :world
+  end
+end
