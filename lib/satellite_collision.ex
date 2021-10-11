8dd66a701202fc75b7fc08318c1e55c489ef3962
brandynbennett <brandynlbennett@gmail.com>
2021-10-11 14:11:22 -0600

    Test if 2 satellites will collide

--- lib/satellite_collision.ex
+++ lib/satellite_collision.ex
@@ -4,8 +4,13 @@ defmodule SatelliteCollision do
   """
 
   @doc """
+  Determine if 2 satellites are too close together
   """
-  def will_collide?() do
-    false
+  def will_collide?(satellite1, satellite2, threshold) do
+    point1 = Satellite.to_cartesian(satellite1)
+    point2 = Satellite.to_cartesian(satellite2)
+    distance = Point.distance(point1, point2)
+    IO.inspect(distance, label: "distance")
+    distance <= threshold
   end
 end
--- test/satellite_collision_test.exs
+++ test/satellite_collision_test.exs
@@ -2,6 +2,38 @@ defmodule SatelliteCollisionTest do
   use ExUnit.Case
 
   test "will_collide? false if satellites wont collide" do
-    assert SatelliteCollision.will_collide?() == false
+    satellite1 = %Satellite{
+      eccentricity: 0.0001131,
+      height: 556.0281731621435,
+      latitude: -27.73707947914896,
+      longitude: -97.17265433167117
+    }
+
+    satellite2 = %Satellite{
+      eccentricity: 0.0003191,
+      height: 408.25189267837504,
+      latitude: -4.444904136046365,
+      longitude: 115.67730280679655
+    }
+
+    assert SatelliteCollision.will_collide?(satellite1, satellite2, 10) == false
+  end
+
+  test "will_collide? true if 2 satellites will collide" do
+    satellite1 = %Satellite{
+      eccentricity: 0.0001131,
+      height: 556.0281731621435,
+      latitude: -27.73707947914896,
+      longitude: -97.17265433167117
+    }
+
+    satellite2 = %Satellite{
+      eccentricity: 0.0003191,
+      height: 556.0281731621435,
+      latitude: -27.73707947914896,
+      longitude: -97.17265433167117
+    }
+
+    assert SatelliteCollision.will_collide?(satellite1, satellite2, 700) == true
   end
 end
